4a36e5c51b1da5c3ca4cfa2e4754d33e7466963d
Tanja Zakurday <zakurdaytanja@gmail.com>
2024-04-26 09:50:38 +0000

    api key hidden

--- src/api/TV-Show.js
+++ src/api/TV-Show.js
@@ -1,18 +1,18 @@
 import axios from "axios"
 import { FAKES, FAKE_REC } from "./Fakes"
-import { BASE_URL, API_KEY_PARAM, BACKDROPBADE_URL } from "./Config"
+import { BASE_URL, BACKDROPBADE_URL } from "./Config"
 
 export class TVShowAPI {
     static async fetchPopulars() {
         //axios always stores the response in .data/await since its asynchrone
-        const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`)
+        const response = await axios.get(`${BASE_URL}tv/popular?api_key=${process.env.REACT_APP_API_KEY_PARAM}`)
         console.log(response.data.results)
         return response.data.results
         //return FAKES
     };
     static async fetchRecommendations(tvShowID) {
         //axios always stores the response in .data/await since its asynchrone
-        const response = await axios.get(`${BASE_URL}tv/${tvShowID}/recommendations${API_KEY_PARAM}`)
+        const response = await axios.get(`${BASE_URL}tv/${tvShowID}/recommendations?api_key=${process.env.REACT_APP_API_KEY_PARAM}`)
         console.log(response.data.results)
         return response.data.results
         //return FAKE_REC
@@ -20,7 +20,7 @@ export class TVShowAPI {
     static async fetchByTitle(title) {
         //axios always stores the response in .data/await since its asynchrone
         const response = await axios.get(
-            `${BASE_URL}search/tv${API_KEY_PARAM}&query=${title}`)
+            `${BASE_URL}search/tv?api_key=${process.env.REACT_APP_API_KEY_PARAM}&query=${title}`)
         console.log(response.data.results)
         return response.data.results
         //return FAKE_REC
